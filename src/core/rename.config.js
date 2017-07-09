4d9232d054d5b293114ba4ea56d2cb22a951ecd1
Александр Юдин <geexup@gmail.com>
2017-07-09 16:47:08 +0300

    Rename function

--- src/core/rename.config.js
+++ src/core/rename.config.js
@@ -1,3 +1,3 @@
 export const DEFAULT_RENAMES = {
-  '/geexup': 'God'
+  'geexup': 'God'
 };
--- src/main/app.js
+++ src/main/app.js
@@ -1,3 +1,23 @@
 import { DEFAULT_RENAMES } from '../core/rename.config';
 
-for (const rename in DEFAULT_RENAMES) console.log(`[Rename] ${rename} -> ${DEFAULT_RENAMES[rename]}`);
+// for (const rename in DEFAULT_RENAMES) console.log(`[Rename] ${rename} -> ${DEFAULT_RENAMES[rename]}`);
+
+function makeRename() {
+  let renamedCount = 0;
+  const names = Array.from(document.querySelectorAll('.im-mess-stack--lnk:not(.renamed)'));
+
+  names.forEach((nameElem) => {
+    const userId = nameElem.href.split('/').filter((elem, indx, arr) => indx === arr.length - 1)[0];
+    
+    if (DEFAULT_RENAMES[userId] !== void 0) {
+      nameElem.innerText = DEFAULT_RENAMES[userId];
+      renamedCount++;
+    }
+    
+    nameElem.classList.add('renamed');
+  });
+
+  if (renamedCount > 0) console.log(`[Rename] ${renamedCount} users was renamed`);
+}
+
+makeRename(); setInterval(makeRename, 300);
